d56389fbb5f3303edb1775f6622d034544f1fd2c
Ilya Bogdanov <zeerayne@yandex.ru>
2019-11-15 18:02:18 +0300

    Removed templates

--- v8webconsole/webconsole/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.HostListView.as_view(), name='host_list'),
-    url(r'^host/(?P<host_id>[0-9]+)/clusters/$', views.ClusterListView.as_view(), name='cluster_list'),
-    url(r'^cluster/(?P<cluster_id>[0-9]+)/infobases/$', views.InfobaseListView.as_view(), name='infobase_list'),
-]
